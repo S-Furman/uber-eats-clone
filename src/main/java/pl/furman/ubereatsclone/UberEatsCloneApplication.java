fcc79eec05bd110be2ee2366a74461a4aca23cb3
S-Furman <szymon.fur@o2.pl>
2023-02-18 17:17:43 +0100

    init commit

---
+++ src/main/java/pl/furman/ubereatsclone/UberEatsCloneApplication.java
@@ -0,0 +1,13 @@
+package pl.furman.ubereatsclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberEatsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberEatsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/furman/ubereatsclone/UberEatsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.furman.ubereatsclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberEatsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
